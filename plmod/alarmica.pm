0a86351a394ebe12673a3e0247330621741e3b64
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2015-08-21 12:08:12 -0400

    Updated libraries

--- plmod/alarmica.pm
+++ plmod/alarmica.pm
@@ -6,10 +6,232 @@ use argola;
 my $resdir;
 my $soundfl;
 my $wait_rate = 3;
+my $hme;
+my $cbkdr;
+my $dbkfl;
+my $ringfun;
+
+my $permitay = 0;
+my $destinay;
+
+$ringfun = \&ringtalert;
+
+$hme = $ENV{"HOME"};
+$cbkdr = $hme . "/.chobakwrap/plmr-randomica";
+$dbkfl = $cbkdr . "/message.txt";
 
 
 $resdir = &argola::srcd;
 $soundfl = $resdir . "/sounds/42095__fauxpress__bell-meditation.mp3";
+
+
+sub ringtalert {
+  my $lc_code;
+  my $lc_ref;
+  my $lc_vol;
+  
+  $lc_ref = $_[0];
+  $lc_code = &randomica::ranstrg(8);
+  $lc_vol = 0.05;
+  while ( !(findmsg($lc_code)) )
+  {
+    &outptex("\n\nRINGING AFTER TASK:\n    " . $$lc_ref{"mesg"});
+    &outptex("  " . $lc_code . " -- Vol=" . $lc_vol);
+    fg_invi_vol($lc_vol);
+    $lc_vol = ( $lc_vol * 1.02 );
+    if ( $lc_vol > 1 ) { $lc_vol = 1; }
+    sleep(2);
+  }
+}
+
+
+
+sub parcesec {
+  my $lc_src;
+  my $lc_sec;
+  my $lc_min;
+  
+  $lc_src = $_[0];
+  &wean($lc_src,60,$lc_sec);
+  &dgits(2,$lc_sec);
+  &wean($lc_src,60,$lc_min);
+  &dgits(2,$lc_min);
+  
+  return ( $lc_src . ":" . $lc_min . ":" . $lc_sec );
+}
+
+sub wean {
+  my $lc_hollow;
+  $_[2] = $_[0] % $_[1];
+  $lc_hollow = int(($_[0] - $_[2]) + 0.2);
+  $_[0] = int(($lc_hollow / $_[1]) + 0.2);
+}
+
+sub dgits {
+  my $lc_src;
+  my $lc_tgo;
+  my $lc_ret;
+  
+  $lc_src = $_[1];
+  $lc_tgo = $_[0];
+  $lc_ret = "";
+  while ( $lc_tgo > 0.5 )
+  {
+    $lc_src = "00" . $lc_src;
+    $lc_ret = chop($lc_src) . $lc_ret;
+    $lc_tgo = int($lc_tgo - 0.8);
+  }
+  
+  $_[1] = $lc_ret;
+}
+
+
+sub setlabel {
+  my $lc_lf;
+  $lc_lf = $_[0];
+  $$lc_lf{"mesg"} = $_[1];
+}
+
+sub howremain {
+  my $lc_now;
+  $lc_now = &nowo;
+  if ( $lc_now >= $_[0] ) { return ( 1 > 2 ); }
+  
+  $_[1] = int(($_[0] - $lc_now) + 0.2);
+  return ( 2 > 1 );
+}
+
+sub justbesure {
+  my $lc_hammer;
+  my $lc_code;
+  my $lc_left;
+  
+  &findmsg("no");
+  $lc_code = &randomica::ranstrg(8);
+  
+  $lc_hammer = int(&nowo + 70.2);
+  
+  while ( &howremain($lc_hammer,$lc_left) )
+  {
+    &outptex("\n\n"
+      . "JUST TO BE SURE YOU WANT TO INTERRUPT THE WHOLE PROCESS:\n"
+      . "  You have " . &parcesec($lc_left) . " to enter: " . $lc_code . "\n"
+      . "  (Or just \"no\" to cancel)"
+    );
+    if ( &findmsg($lc_code) )
+    {
+      &outptex("\nOKAY --- THE PROCESS IS OVER WITH:");
+      exit(0);
+    }
+    if ( &findmsg("no") )
+    {
+      return;
+    }
+    sleep(1);
+  }
+}
+
+sub wait {
+  my $lc_code;
+  my $lc_prwcode;
+  my $lc_ref;
+  my $lc_start;
+  my $lc_projend;
+  my $lc_mesg;
+  my $lc_endure;
+  
+  $lc_ref = $_[0];
+  $lc_start = $$lc_ref{"at"};
+  $lc_mesg = $$lc_ref{"mesg"};
+  $lc_projend = int($lc_start + ( $_[1] * 60 ) + $_[2] + 0.2);
+  $$lc_ref{"at"} = $lc_projend;
+  $lc_code = &randomica::ranstrg(8);
+  $lc_prwcode = &randomica::ranstrg(8);
+  while ( &howremain($lc_projend,$lc_endure) )
+  {
+    if ( &findmsg($lc_code) )
+    {
+      $$lc_ref{"at"} = &nowo;
+      &outptex("\n\nTASK ENDED EARLY: (Ringing part will be skipped.)");
+      return;
+    }
+    if ( &findmsg($lc_prwcode) )
+    {
+      &justbesure;
+    }
+    &outptex("\n\nTASK: " . $lc_mesg . ":\n" . $lc_code . " -- " . &parcesec($lc_endure));
+    &outptex("(Process-wide interrupt: " . $lc_prwcode . ")");
+    if ( $lc_endure > 15 )
+    {
+      &do_caf(40);
+      sleep(5);
+      if ( &findmsg($lc_code) )
+      {
+        $$lc_ref{"at"} = &nowo;
+        &outptex("\n\nTASK ENDED EARLY: (Ringing part will be skipped.)");
+        return;
+      }
+      sleep(4);
+    }
+    sleep(1);
+  }
+  
+  &$ringfun($lc_ref);
+  
+}
+
+
+
+sub regmsg {
+  my $lc_cm;
+  $lc_cm = "echo";
+  &argola::wraprg_lst($lc_cm,(&nowo . ":" . $_[0]));
+  $lc_cm .= " >> ";
+  &argola::wraprg_lst($lc_cm,$dbkfl);
+  system("mkdir","-p",$cbkdr);
+  system($lc_cm);
+}
+
+sub findmsg {
+  my $lc_cm;
+  my $lc_rs;
+  my $lc_er;
+  my @lc_al;
+  my $lc_each;
+  my $lc_found;
+  my $lc_when;
+  my $lc_what;
+  
+  $lc_found = 0;
+  
+  $lc_er = int((&nowo - ( 60 * 60 )) + 0.2);
+  $lc_cm = "cat";
+  &argola::wraprg_lst($lc_cm,$dbkfl);
+  $lc_cm .= " 2> /dev/null";
+  $lc_rs = `$lc_cm`;
+  system("rm","-rf",$dbkfl);
+  @lc_al = split(/\n/,$lc_rs);
+  foreach $lc_each (@lc_al)
+  {
+    ($lc_when,$lc_what) = split(/:/,$lc_each,2);
+    if ( $lc_what eq $_[0] )
+    {
+      $lc_found = 10;
+    } else {
+      if ( $lc_when > $lc_er )
+      {
+        $lc_cm = "echo";
+        &argola::wraprg_lst($lc_cm,$lc_each);
+        $lc_cm .= " >> ";
+        &argola::wraprg_lst($lc_cm,$dbkfl);
+        system("mkdir","-p",$cbkdr);
+        system($lc_cm);
+      }
+    }
+  }
+  
+  return ( $lc_found > 5 );
+}
 
 sub shlc_caf {
   my $lc_ret;
@@ -104,14 +326,14 @@ sub advance_by_s {
   $lc_to = int($lc_at + $_[1] + 0.2);
   $$lc_ref{"at"} = $lc_to;
   
-  system("echo",$lc_at . " : " . $lc_to);
+  &outptex($lc_at . " : " . $lc_to);
   
   &do_caf(30);
   
   $lc_lft = int(($lc_to - &nowo) + 0.2);
   while ( $lc_lft > 0.5 )
   {
-    system("echo",": " . $lc_lft);
+    &outptex(": " . $lc_lft);
     if ( $lc_lft > 10.5 )
     {
       sleep(8);
@@ -121,5 +343,33 @@ sub advance_by_s {
     $lc_lft = int(($lc_to - &nowo) + 0.2);
   }
 }
+
+sub outptex {
+  my $lc_rg;
+  my $lc_cm;
+  if ( $permitay < 5 )
+  {
+    foreach $lc_rg (@_)
+    {
+      system("echo",$lc_rg);
+    }
+    return;
+  }
+  foreach $lc_rg (@_)
+  {
+    $lc_cm = "echo";
+    &argola::wraprg_lst($lc_cm,$lc_rg);
+    $lc_cm .= " >>";
+    &argola::wraprg_lst($lc_cm,$destinay);
+    system($lc_cm);
+  }
+}
+
+sub setout {
+  $permitay = 10;
+  $destinay = $_[0];
+}
+
+
 
 1;
--- plmod/randomica.pm
+++ plmod/randomica.pm
@@ -2,13 +2,6 @@ package randomica;
 use strict;
 
 my @sordonic;
-my $hme;
-my $cbkdr;
-my $dbkfl;
-
-$hme = $ENV{"HOME"};
-$cbkdr = $hme . "/.chobakwrap/plmr-randomica";
-$dbkfl = $cbkdr . "/message.txt";
 
 @sordonic = ('a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','v','w','x','z');
 
@@ -20,7 +13,7 @@ $dbkfl = $cbkdr . "/message.txt";
 {
   my $lc_a;
   $lc_a = @sordonic;
-  $lc_a = int(($lc_a * 3) + 0.2);
+  $lc_a = int(($lc_a * 9) + 0.2);
   &ranstrg($lc_a);
 }
 
@@ -28,14 +21,18 @@ $dbkfl = $cbkdr . "/message.txt";
 sub un_char {
   my $lc_zla;
   my $lc_chr;
+  my @lc_prn;
+  
+  @lc_prn = ();
   
   $lc_zla = int(rand(6));
   while ( $lc_zla > 0.5 )
   {
     $lc_chr = shift(@sordonic);
-    @sordonic = (@sordonic,$lc_chr);
+    @lc_prn = ($lc_chr,@lc_prn);
     $lc_zla = int($lc_zla - 0.8);
   }
+  @sordonic = (@sordonic,@lc_prn);
   $lc_chr = shift(@sordonic);
   @sordonic = (@sordonic,$lc_chr);
   return $lc_chr;
